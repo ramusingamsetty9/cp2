ddc5dd4bd627e7b72fb4a6335c9455c0b72147b0
Ramu Singamsetty <65004863+ramusingamsetty9@users.noreply.github.com>
2024-03-01 16:47:02 +0530

    Update cp2.sh

--- cp2.sh
+++ cp2.sh
@@ -14,7 +14,7 @@ else
     echo "Invalid response. Please enter 'yes' or 'no'."
 fi
 #Creation of new  folder
-folder_name="RTA_$(date +\%Y\%m\%d_\%H\%M\%S)"
+folder_name="RTA_$(date +\%d\%m\%Y_\%H\%M\%S)"
 mkdir "$folder_name"
 
 #creation of new  folder
@@ -60,12 +60,42 @@ echo "attack surface analysed and stored to devices.txt"
 
 #web service identification
 echo "web servers are being analysed"
-nmap --open -p 80 192.168.1.1/24 |grep -E -o "([0-9]{1,3}\.){3}[0-9]{1,3}"| awk '!seen[$0]++' | cat > web.txt 
+nmap --open -p 80 "$ip_address"/$cidr | grep -E -o "([0-9]{1,3}\.){3}[0-9]{1,3}" | awk '!seen[$0]++' | cat > web_ip.txt
 
 #web security testing
-nuclei -l web.txt -t http/ | cat > web_security_report.txt
-echo "All Web app scans completed."
+# Specify the input file containing IP addresses
+input_file="web_ip.txt"
 
-#Web directory testing
-mkdir web_dir
-cd web_dir
+# Specify the output directory
+output_directory="nuclei_scans"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_directory"
+
+# Run Nuclei scan for each URL in the input file
+while IFS= read -r url || [[ -n "$url" ]]; do
+    output_file="$output_directory/${url//[:\/]/_}_nuclei_report.txt"
+    nuclei -u "$url" -o "$output_file"
+    echo "Nuclei scan for $url completed. Report saved in '$output_file'."
+done < "$input_file"
+
+echo "All Nuclei scans completed. Reports saved in the '$output_directory' directory."
+
+
+#DIR_ENUM
+# Specify the input file containing IP addresses
+input_file="web_ip.txt"
+
+# Specify the output directory
+output_directory="dirb_reports"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_directory"
+# Run dirb scan for each IP address in the input file
+while IFS= read -r ip || [[ -n "$ip" ]]; do
+    output_file="$output_directory/${ip}_web_dir.txt"
+    dirb "http://$ip" -o "$output_file"
+    echo "Dirb scan for $ip completed. Report saved in '$output_file'."
+done < "$input_file"
+
+echo "All dirb scans completed. Reports saved in the '$output_directory' directory."
